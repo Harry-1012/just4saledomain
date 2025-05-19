6fe230ee9c81518cf1f2d62472f61eaed44bef84
Harry <haoleiqin@qq.com>
2025-05-19 16:48:40 +0800

    New commit, clear history

---
+++ ihogucom_res/shici.js
@@ -0,0 +1,185 @@
+let shiciListAll20240402 = [
+  {
+    title: "诫子书",
+    item: [
+      "夫君子之行，静以修身，俭以养德。非淡泊无以明志，非宁静无以致远。夫学须静也，才须学也，非学无以广才，非志无以成学。",
+      "淫慢则不能励精，险躁则不能治性。年与时驰，意与日去，遂成枯落，多不接世，悲守穷庐，将复何及",
+    ],
+  },
+  {
+    title: "出师表",
+    item: [
+      "先帝创业未半而中道崩殂，今天下三分，益州疲弊，此诚危急存亡之秋也。然侍卫之臣不懈于内，忠志之士忘身于外者，盖追先帝之殊遇，欲报之于陛下也。诚宜开张圣听，以光先帝遗德，恢弘志士之气，不宜妄自菲薄，引喻失义，以塞忠谏之路也。",
+      "宫中府中，俱为一体；陟罚臧否，不宜异同。若有作奸犯科及为忠善者，宜付有司论其刑赏，以昭陛下平明之理，不宜偏私，使内外异法也。",
+      "侍中、侍郎郭攸之、费祎、董允等，此皆良实，志虑忠纯，是以先帝简拔以遗陛下。愚以为宫中之事，事无大小，悉以咨之，然后施行，必能裨补阙漏，有所广益。",
+      "将军向宠，性行淑均，晓畅军事，试用于昔日，先帝称之曰能，是以众议举宠为督。愚以为营中之事，悉以咨之，必能使行阵和睦，优劣得所。",
+      "亲贤臣，远小人，此先汉所以兴隆也；亲小人，远贤臣，此后汉所以倾颓也。先帝在时，每与臣论此事，未尝不叹息痛恨于桓、灵也。侍中、尚书、长史、参军，此悉贞良死节之臣，愿陛下亲之信之，则汉室之隆，可计日而待也。",
+      "臣本布衣，躬耕于南阳，苟全性命于乱世，不求闻达于诸侯。先帝不以臣卑鄙，猥自枉屈，三顾臣于草庐之中，咨臣以当世之事，由是感激，遂许先帝以驱驰。后值倾覆，受任于败军之际，奉命于危难之间，尔来二十有一年矣。",
+      "先帝知臣谨慎，故临崩寄臣以大事也。受命以来，夙夜忧叹，恐托付不效，以伤先帝之明；故五月渡泸，深入不毛。今南方已定，兵甲已足，当奖率三军，北定中原，庶竭驽钝，攘除奸凶，兴复汉室，还于旧都。此臣所以报先帝而忠陛下之职分也。至于斟酌损益，进尽忠言，则攸之、祎、允之任也。",
+    ],
+  },
+  {
+    title: "行路难 李白",
+    item: [
+      "金樽清酒斗十千，玉盘珍羞直万钱。",
+      "停杯投箸不能食，拔剑四顾心茫然。",
+      "闲来垂钓碧溪上，忽复乘舟梦日边。",
+      "行路难，行路难，多歧路，今安在？",
+      "长风破浪会有时，直挂云帆济沧海。",
+    ],
+  },
+  {
+    title: "浪淘沙·北戴河",
+    item: [
+      "大雨落幽燕，白浪滔天，秦皇岛外打鱼船。一片汪洋都不见，知向谁边？",
+      "往事越千年，魏武挥鞭，东临碣石有遗篇。萧瑟秋风今又是，换了人间。",
+    ],
+  },
+  {
+    title: "水调歌头·游泳",
+    item: [
+      "才饮长沙水，又食武昌鱼。万里长江横渡，极目楚天舒。不管风吹浪打，胜似闲庭信步，今日得宽馀。子在川上曰：逝者如斯夫！",
+      "风樯动，龟蛇静，起宏图。一桥飞架南北，天堑变通途。更立西江石壁，截断巫山云雨，高峡出平湖。神女应无恙，当惊世界殊。",
+    ],
+  },
+  {
+    title: "采桑子·重阳",
+    item: [
+      "人生易老天难老，岁岁重阳。今又重阳，战地黄花分外香。",
+      "一年一度秋风劲，不似春光。胜似春光，寥廓江天万里霜。",
+    ],
+  },
+  {
+    title: "青玉案·元夕",
+    item: [
+      "东风夜放花千树，更吹落、星如雨。宝马雕车香满路。凤箫声动，玉壶光转，一夜鱼龙舞。",
+      "蛾儿雪柳黄金缕，笑语盈盈暗香去。众里寻他千百度，蓦然回首，那人却在，灯火阑珊处。",
+    ],
+  },
+  {
+    title: "永遇乐·京口北固亭怀古",
+    item: [
+      "千古江山，英雄无觅孙仲谋处。舞榭歌台，风流总被雨打风吹去。斜阳草树，寻常巷陌，人道寄奴曾住。想当年，金戈铁马，气吞万里如虎。",
+      "元嘉草草，封狼居胥，赢得仓皇北顾。四十三年，望中犹记，烽火扬州路。可堪回首，佛狸祠下，一片神鸦社鼓。凭谁问：廉颇老矣，尚能饭否？",
+    ],
+  },
+  {
+    title: "破阵子·为陈同甫赋壮词以寄之",
+    item: [
+      "醉里挑灯看剑，梦回吹角连营。八百里分麾下炙，五十弦翻塞外声，沙场秋点兵。",
+      "马作的卢飞快，弓如霹雳弦惊。了却君王天下事，赢得生前身后名。可怜白发生！",
+    ],
+  },
+  {
+    title: "马说",
+    item: [
+      "世有伯乐，然后有千里马。千里马常有，而伯乐不常有。故虽有名马，祗辱于奴隶人之手，骈死于槽枥之间，不以千里称也。",
+      "马之千里者，一食或尽粟一石。食马者不知其能千里而食也。是马也，虽有千里之能，食不饱，力不足，才美不外见，且欲与常马等不可得，安求其能千里也？",
+      "策之不以其道，食之不能尽其材，鸣之而不能通其意，执策而临之，曰：“天下无马！”呜呼！其真无马邪？其真不知马也！",
+    ],
+  },
+];
+
+let shiciListAll20240402_en = [
+  {
+    title: "Admonitions for the Son",
+    item: [
+      "The conduct of a gentleman should be quiet and self-cultivating, frugal and nurturing virtue. Without being indifferent, one cannot clarify aspirations; without being tranquil, one cannot reach far. Learning requires tranquility; talent requires learning. Without learning, one cannot broaden talent; without aspiration, one cannot accomplish learning.",
+      "Indulgence and laziness cannot stimulate diligence; recklessness and impatience cannot govern one's nature. As years and time rush by, aspirations fade away, resulting in desolation and decline, often disconnected from the world. It's lamentable to dwell in a desolate hut; what hope is there for recovery?",
+    ],
+  },
+  {
+    title: "Memorial on Behalf of the Soldiers",
+    item: [
+      "The late emperor, who laid the foundation, passed away prematurely while his work was still incomplete. Today, the empire is divided into three, and Yizhou is exhausted. This is truly a critical moment of danger and survival. Yet, those who serve in the court are unwaveringly committed internally, and those with loyal aspirations forget themselves externally. They are driven by the memory of the late emperor's exceptional treatment and wish to repay Your Majesty. It is appropriate to open the doors to wise counsel, to illuminate the late emperor's virtues, to restore the spirit of the loyalists, and not to underestimate oneself or distort analogies, lest loyal remonstrances be blocked.",
+      "Inside the palace and the government offices, all are part of a unified entity; the punishment and appraisal of officials should be consistent. If there are those who commit crimes or perform acts of loyalty and goodness, they should be judged by the authorities for their merits and punishments, to demonstrate Your Majesty's fairness and justice, avoiding partiality and ensuring consistency in law enforcement both internally and externally.",
+      "Guo Youzhi, Fei Yi, Dong Yun, and others, are all upright individuals, pure in loyalty and thoughts. That is why the late emperor selected and left them to Your Majesty. I believe that all matters within the palace, regardless of size, should be consulted with them before execution, which will surely help fill the gaps and bring benefits.",
+      "General Xiang Chong possesses noble qualities, conducts himself with integrity, and understands military affairs thoroughly. Having been tested in the past, the late emperor praised him as capable, and thus, he was unanimously recommended as a commander. I suggest consulting him on military matters, which will ensure harmony and efficiency in the camps and appropriate assignments for each individual.",
+      "Favoring wise ministers and distancing oneself from petty people was the reason for the prosperity of the Former Han dynasty. Favoring petty people and distancing oneself from wise ministers was the reason for the decline of the Later Han dynasty. When the late emperor was alive, he often discussed this matter with me, always sighing and regretting the times of Emperor Huan and Emperor Ling. Those serving as attendants, secretaries, chief clerks, and aides were all loyal subjects with unwavering integrity. I hope Your Majesty will trust and rely on them, then the prosperity of the Han dynasty can be expected.",
+    ],
+  },
+  {
+    title: "The Road of Life - Li Bai",
+    item: [
+      "With a thousand measures of clear wine in golden goblets, and precious dishes worth ten thousand in jade plates.",
+      "Stopping the cup and putting down the chopsticks, unable to eat, I draw my sword and look around in confusion.",
+      "In leisure, I fish in the blue stream above, then suddenly I board a boat and dream by the edge of the sun.",
+      "The road of life, the road of life, with many diverging paths, where am I now?",
+      "The strong winds and waves will sometimes come, and I'll sail directly through the clouds and sail across the boundless sea.",
+    ],
+  },
+  {
+    title: "Waves Washing Sand - Beidaihe",
+    item: [
+      "Great rains fall in Youyan, white waves surge to the sky, and fishing boats sail beyond Qinhuangdao. All that is left is a vast expanse of water, but who knows where it goes?",
+      "A thousand years of past events, with Wei Wu brandishing his whip and overlooking the sea, leaving behind remnants at Jieshi. The bleak autumn wind is once again here, but the world has changed.",
+    ],
+  },
+  {
+    title: "Water Melody - Swimming",
+    item: [
+      "Just drank water from Changsha, and ate fish from Wuchang. Crossing the mighty Yangtze River for thousands of miles, I gaze far across the Chu skies. Regardless of the wind and waves, it's better than strolling in a leisurely courtyard. Today, I've found some extra leisure. As Confucius said while crossing the river: 'Gone like this is the passage of time!'",
+      "The wind moves the sail, the turtle and snake are still, and a grand vision is conceived. A bridge spans the north and south, turning a natural barrier into a convenient passage. Erecting stone walls along the Xijiang, cutting off the clouds and rain of Mount Wushan, and the high gorges opening into the Ping Lake. The goddess should be safe and sound, surprising the world with her miracles.",
+    ],
+  },
+  {
+    title: "Picking Mulberries - Double Ninth Festival",
+    item: [
+      "Life is easy to grow old, but the heavens are hard to grow old. The Double Ninth Festival comes year after year, and today, it's here again. The yellow flowers on the battlefield are exceptionally fragrant.",
+      "The autumn wind is strong every year, not like the spring sunshine. It's better than the spring sunshine, as vast frost covers the river and sky.",
+    ],
+  },
+  {
+    title: "Blue Jade Table - Mid-Autumn Festival",
+    item: [
+      "In the east wind, flowers fall from a thousand trees, and stars fall like rain. The precious horse-drawn carriages fill the road with fragrance. The phoenix flutes play, and the jade urn shines, with fish and dragons dancing all night long.",
+      "Moths and willows with golden threads, laughing and speaking with a hidden fragrance. Among the crowds, searching thousands of times, suddenly turning back, that person is there, where the lights are dim.",
+    ],
+  },
+  {
+    title: "Eternal Joy - Reminiscing Ancient Times at Jingkou Beiguting",
+    item: [
+      "The mountains and rivers of the past are where heroes are not found, like Sun Zhongmou's place. The pavilions and stages, the elegance is always swept away by the rain and wind. The setting sun, the grass and trees, the ordinary lanes, where I once lived as a guest. Thinking of the past, with golden horses and iron horses, swallowing thousands of miles like a tiger.",
+      "In the Yuanjia period, things were rushed, and wolves were made to live in seclusion, winning panic glances to the north. Forty-three years, looking back, I still remember, the beacon fires along the Yangzhou road. Looking back now, beneath the temple of the fox spirit, a group of crows gather. Who would dare ask: Lin Po is old, can he still eat?",
+    ],
+  },
+  {
+    title:
+      "Breaking the Enemy's Formation - Composing a Vigorous Poem for Chen Tongfu",
+    item: [
+      "Drunk, I light a lamp and look at the sword, dreaming of battles and sounding horns. Eight hundred miles under my command, fifty strings flip the sounds beyond the borders. On the battlefield, autumn raises its troops.",
+      "The horse of Diefu flies fast, the bowstring is like thunder, and the string startles. All the emperor's affairs are concluded, winning fame before and after life. It's a pity that white hair grows!",
+    ],
+  },
+  {
+    title: "Horse Talk",
+    item: [
+      "There are horse trainers in the world, and then there are thousand-mile horses. Thousand-mile horses are often present, but horse trainers are not always present. Thus, even though there are renowned horses, they are humiliated in the hands of slave men, dying between mangers and racks, not worthy of being called thousand-mile horses.",
+      "As for the thousand-mile horse, it may consume a stone of grain in a single meal. Those who feed horses do not know that they can travel a thousand miles while eating. This horse, even though it has the ability to travel a thousand miles, cannot be satisfied with food, lacks strength, and its beauty is not apparent. Even if it desires to be equal to an ordinary horse, it cannot be obtained. Where can one find its ability to travel a thousand miles?",
+      "Without guiding it on the right path, without feeding it to fully utilize its abilities, and without understanding its desires when it neighs, holding the reins and facing it, saying, 'There are no horses in the world!' Alas! Is there really no horse? Do they really not understand horses?",
+    ],
+  },
+];
+
+let configDataPage20240405CN = {
+  title: "好好学习 天天向上-每日学习心得",
+  welcome: "🧙欢迎光临harrywork.com🤗",
+  gametips: `🤓您可以发个呆 或者🎮玩一会小恐龙快跑&nbsp;&nbsp;&nbsp;&nbsp;<br />按<span>空格(space)</span><span>上(↑)</span>
+      <span>下(↓)</span>键试试 <br/>`,
+  go2blogwork: ` 前往:
+      <a href="/work" target="_blank">作品</a> 
+      <a href="/blog"  target="_blank">博客</a>`,
+  meirishici: "📖每日诗词",
+};
+
+
+let configDataPage20240405EN = {
+  title: '每日学习心得 Study hard, improve every day - Daily Learning Reflections',
+  welcome: "🧙Welcome to harrywork.com🤗",
+  gametips: `You can zone out 🤓 or play a quick round of the dinosaur game🎮&nbsp;&nbsp;&nbsp;&nbsp;<br />Press<span>space</span><span>up(↑)</span>
+      <span>down(↓)</span>to try <br/>`,
+  go2blogwork: ` Go to:
+      <a href="/work" target="_blank">Project</a> 
+      <a href="/blog"  target="_blank">Blog</a>`,
+  meirishici: "📖Daily Poetry",
+};
